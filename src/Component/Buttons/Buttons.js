306a743539da39b3c8e4a2e35d39aef6663f235c
khalid nidal <kahlid_s@hottmail.com>
2023-09-02 01:25:50 +0300

    react

---
+++ src/Component/Buttons/Buttons.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Buttons() {
+  return (
+    <>
+        
+        <div className=''>
+           <button className=' shadow-blue-900  shadow-md m-2 capitalize bg-indigo-500 text-white rounded-lg px-2 p-1 hover:bg-slate-400 hover:text-black'>tag button</button>
+        </div>
+
+    </>
+  )
+}
